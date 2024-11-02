1e8ca00f8ee69eb474663ade3e895f8d1b05df82
Oracle Public Cloud User <opc@arma3-sog.subnet12151638.vcn12151638.oraclevcn.com>
2024-11-02 22:01:13 +0000

    update.sh: Add script
    The systemd service depends on this script, so it should be tracked in
    git.

---
+++ update.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+/home/opc/steamcmd/steamcmd.sh +force_install_dir /data/dayz/ +login bxlentpartyon +app_update 223350 +workshop_download_item 221100 1559212036 +workshop_download_item 221100 1828439124 +workshop_download_item 221100 1854626456 +workshop_download_item 221100 1646187754 +quit
