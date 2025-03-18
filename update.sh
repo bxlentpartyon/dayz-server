4e40a86b9ec62c72b78d949d2133019e5fe52674
Oracle Public Cloud User <opc@arma3-sog.subnet12151638.vcn12151638.oraclevcn.com>
2025-03-18 16:13:50 +0000

    update.sh: Copy config files back into server root after update

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
 /home/opc/steamcmd/steamcmd.sh +force_install_dir /data/dayz/ +login bxlentpartyon +app_update 223350 validate +workshop_download_item 221100 1559212036 +workshop_download_item 221100 1828439124 +workshop_download_item 221100 1854626456 +workshop_download_item 221100 1646187754 +quit
+rsync -avHx /home/opc/dayz_server_git/dayz_root /data/dayz
