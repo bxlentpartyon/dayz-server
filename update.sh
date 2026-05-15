56351a5e03538ea383639c3f14dd89876abef04d
Oracle Public Cloud User <opc@arma3-sog.subnet12151638.vcn12151638.oraclevcn.com>
2026-05-15 01:29:17 +0000

    update: Actually store off log output

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-/home/opc/steamcmd/steamcmd.sh +force_install_dir /data/dayz/ +login bxlentpartyon +app_update 223350 validate +workshop_download_item 221100 1559212036 +workshop_download_item 221100 1828439124 +workshop_download_item 221100 1854626456 +workshop_download_item 221100 1646187754 +quit
+/home/opc/steamcmd/steamcmd.sh +force_install_dir /data/dayz/ +login bxlentpartyon +app_update 223350 validate +workshop_download_item 221100 1559212036 +workshop_download_item 221100 1828439124 +workshop_download_item 221100 1854626456 +workshop_download_item 221100 1646187754 +quit &> /data/dayz/update.log
 rsync -avHx /home/opc/dayz_server_git/dayz_root/ /data/dayz
 chcon -t bin_t /data/dayz/DayZServer
